c2beb44a060e046459649390db3037e72647a993
Collin Williams <6917990+bluedragon1221@users.noreply.github.com>
2023-11-13 11:44:57 -0600

    Script for Attrs of Quadratic

--- python/math/attrs_of_quad.py
+++ python/math/attrs_of_quad.py
@@ -1,14 +1,16 @@
-from math import *
+def sqrt(i: int):
+    return i**(1/2)
 
 def attrs(a, b, c):
     magic = -b / (2 * a)
 
-    magic_plug = (a*(magic**2)) + (b*magic) + c
+    magic_plug = (a * (magic**2)) + (b * magic) + c
 
-    # roots = [
-    #     (-b + sqrt(b**2 - (4 * a * c))) / (2 * a),
-    #     (-b - sqrt(b**2 - (4 * a * c))) / (2 * a)
-    # ]
+    the_sqrt = sqrt(b**2 - (4 * a * c))
+    roots = [
+        (-b + the_sqrt) / (2 * a),
+        (-b - the_sqrt) / (2 * a)
+    ]
 
     if a > 0:
         direction = ">="
@@ -17,18 +19,16 @@ def attrs(a, b, c):
         direction = "<="
         point = "max"
 
-
-
     return {
         "y-int": (0, c),
         "aos": f"x = {magic}",
         "vertex": (magic, magic_plug),
-        # "x-int": roots,
-        # "domain": "all reals",
-        # "range": f"y {direction} {magic_plug}"
+        "x-int": roots,
+        "domain": "all reals",
+        "range": f"y {direction} {magic_plug}",
         "max/min": point
     }
 
 if __name__ == "__main__":
-    # print(attrs(-2, 0, 0))
+    print(attrs(-2, 0, 0))
     print(attrs(1, -4, 4))
