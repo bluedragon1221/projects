5d5b2c4630cc98b191e7e20b3269fdd7961657f0
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2024-08-11 08:31:59 -0500

    htmacro

---
+++ lithp/htmacro/flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    cl-nix-lite.url = "github:hraban/cl-nix-lite";
+  };
+
+  outputs = { nixpkgs, cl-nix-lite, ... }: let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system}.extend cl-nix-lite.overlays.default;
+    in {
+      packages.default = with pkgs.lispPackagesLite; lispScript {
+        name = "htmacro";
+        dependencies = [ "str" "fn" "arrow-macros" ];
+        src = "./main.lisp";
+      };
+    };
+}
+
